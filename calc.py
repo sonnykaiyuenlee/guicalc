09c5cb53d753068332330cbfa5e7bdcafca3a9db
sonnykaiyuenlee <sonny@uchicago.edu>
2021-07-08 13:15:30 -0500

    working on alignment

--- calc.py
+++ calc.py
@@ -1,7 +1,10 @@
-import wx 
+import wx
+from wx.core import DefaultPosition, DefaultSize, NullColour, NullFont, TE_MULTILINE, TE_READONLY, TextAttr, ColourDatabase
 app = wx.App()
 
 frm = wx.Frame(None, title="Sonny's GUI Calculator")
+
+error_message = "Invalid input! Try again!"
 
 #welcome message
 pop_message = "Welcome to Sonny's Calculator! Click OK to continue, CANCEL to exit"
@@ -12,19 +15,47 @@ popup_result = popup_message.ShowModal()
 if popup_result == wx.ID_CANCEL:
     wx.Exit()
 
-entry_message = "Please Enter the Expression You Are Looking to Solve"
-entry_caption = "ENTER EXPRESSION"
-default_value = ""
-entry = wx.TextEntryDialog(None, entry_message, entry_caption, default_value)
-entry_result = entry.ShowModal()    
+text_entry = ""
+expression_style = wx.TE_READONLY | wx.TE_MULTILINE |wx.TE_RICH | wx.TE_PROCESS_ENTER
+answer_style = wx.TE_READONLY | wx.TE_MULTILINE |wx.TE_RICH | wx.TE_PROCESS_ENTER | wx.TE_RIGHT
+display_expression = wx.TextCtrl(frm, -1, text_entry, DefaultPosition, DefaultSize , expression_style)
+while 1:
+    display_expression.SetDefaultStyle(TextAttr(NullColour, NullColour, NullFont, wx.TEXT_ALIGNMENT_LEFT))
+    entry_message = "Please Enter the Expression You Are Looking to Solve"
+    entry_caption = "ENTER EXPRESSION"
+    default_value = ""
+    entry = wx.TextEntryDialog(None, entry_message, entry_caption, default_value)
+    #fix the display message, it currently cuts off the last word
+    entry_result = entry.ShowModal()    
 
-if entry_result == wx.ID_CANCEL:
-    #do we want to double check that the user wants to exit?
-    wx.Exit()
+    if entry_result == wx.ID_CANCEL:
+        #do we want to double check that the user wants to exit?
+        wx.Exit()
+    else:
+        user_text = entry.GetValue()
+        if user_text == default_value:
+            #this means nothing was entered
+            user_text = error_message
+
+        user_text += '\n'
+        #new_line = '\n'
+
+        #display_expression = wx.TextCtrl(frm, -1, text_entry, DefaultPosition, wx.Size(100,100) , wx.TE_MULTILINE | wx.TE_READONLY)
+        display_expression.AppendText(user_text)
+
+        #here is where we should pass the text to a parsing function, 
+        #and then perform the operation, get the answer, 
+
+        answer = "answer!\n"
+        
+        display_expression.SetDefaultStyle(TextAttr(NullColour, NullColour, NullFont, wx.TEXT_ALIGNMENT_RIGHT))
+        
+        
+
+        #wx.StaticText(frm, -1, new_line)
 
 
-
-frm.Centre(wx.BOTH)
-frm.Show()
+    frm.Centre(wx.BOTH)
+    frm.Show()
 
 app.MainLoop()
