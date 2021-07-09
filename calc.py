3f8efa2e78029bf7640da95b73d070ab69ffe4da
sonnykaiyuenlee <sonny@uchicago.edu>
2021-07-09 16:56:15 -0500

    still working on alignment

--- calc.py
+++ calc.py
@@ -6,6 +6,10 @@ frm = wx.Frame(None, title="Sonny's GUI Calculator")
 
 error_message = "Invalid input! Try again!"
 
+colordb = wx.ColourDatabase()
+blue = colordb.Find("BLUE")
+green = colordb.Find("GREEN")
+
 #welcome message
 pop_message = "Welcome to Sonny's Calculator! Click OK to continue, CANCEL to exit"
 pop_caption = "WELCOME"
@@ -16,11 +20,15 @@ if popup_result == wx.ID_CANCEL:
     wx.Exit()
 
 text_entry = ""
-expression_style = wx.TE_READONLY | wx.TE_MULTILINE |wx.TE_RICH | wx.TE_PROCESS_ENTER
+expression_style = wx.TE_READONLY | wx.TE_MULTILINE |wx.TE_RICH | wx.TE_PROCESS_ENTER | wx.TE_LEFT
 answer_style = wx.TE_READONLY | wx.TE_MULTILINE |wx.TE_RICH | wx.TE_PROCESS_ENTER | wx.TE_RIGHT
 display_expression = wx.TextCtrl(frm, -1, text_entry, DefaultPosition, DefaultSize , expression_style)
 while 1:
-    display_expression.SetDefaultStyle(TextAttr(NullColour, NullColour, NullFont, wx.TEXT_ALIGNMENT_LEFT))
+    d_expression = wx.TextAttr(blue, NullColour, NullFont, wx.TEXT_ALIGNMENT_RIGHT)
+    display_expression.SetStyle(-1, -1, d_expression)
+    style = display_expression.GetWindowStyle()
+    display_expression.SetWindowStyle(style & ~wx.TEXT_ALIGNMENT_RIGHT | wx.TEXT_ALIGNMENT_LEFT)
+    
     entry_message = "Please Enter the Expression You Are Looking to Solve"
     entry_caption = "ENTER EXPRESSION"
     default_value = ""
@@ -42,13 +50,13 @@ while 1:
 
         #display_expression = wx.TextCtrl(frm, -1, text_entry, DefaultPosition, wx.Size(100,100) , wx.TE_MULTILINE | wx.TE_READONLY)
         display_expression.AppendText(user_text)
-
+        display_expression.SetDefaultStyle(TextAttr(green, NullColour, NullFont, wx.TEXT_ALIGNMENT_CENTER))
         #here is where we should pass the text to a parsing function, 
         #and then perform the operation, get the answer, 
 
         answer = "answer!\n"
+        display_expression.AppendText(answer)
         
-        display_expression.SetDefaultStyle(TextAttr(NullColour, NullColour, NullFont, wx.TEXT_ALIGNMENT_RIGHT))
         
         
 
